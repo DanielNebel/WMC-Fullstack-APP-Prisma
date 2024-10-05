7285c9044b62d756543be7863ff8da5937e1a3a0
DanielNebel <Neb210684@spengergasse.at>
2024-10-05 15:01:58 +0200

    Part3 until Provide the users to the user panel

--- app/components/form-field.tsx
+++ app/components/form-field.tsx
@@ -36,20 +36,20 @@ interface FormFieldProps {
     </>
 }
 
-  // export function FormField({ htmlFor, label, type = 'text', value, onChange = () => {} }: FormFieldProps) {
-  //   return (
-  //     <>
-  //       <label htmlFor={htmlFor} className="text-blue-600 font-semibold">
-  //         {label}
-  //       </label>
-  //       <input
-  //         onChange={onChange}
-  //         type={type}
-  //         id={htmlFor}
-  //         name={htmlFor}
-  //         className="w-full p-2 rounded-xl my-2"
-  //         value={value}
-  //       />
-  //     </>
-  //   )
-  // }
+//   export function FormField({ htmlFor, label, type = 'text', value, onChange = () => {} }: FormFieldProps) {
+//     return (
+//       <>
+//         <label htmlFor={htmlFor} className="text-blue-600 font-semibold">
+//           {label}
+//         </label>
+//         <input
+//           onChange={onChange}
+//           type={type}
+//           id={htmlFor}
+//           name={htmlFor}
+//           className="w-full p-2 rounded-xl my-2"
+//           value={value}
+//         />
+//       </>
+//     )
+//   }
